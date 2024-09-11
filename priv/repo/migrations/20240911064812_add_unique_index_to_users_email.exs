a7ea1e14f986837b71a08702607c0db92beaa05e
G-ordon <gordonochieng5454@gmail.com>
2024-09-11 17:22:30 +0300

    modified the unique_index_to_users_email to prevent the conflicting issues on ecto migration, since it seems they were two

--- priv/repo/migrations/20240911064812_add_unique_index_to_users_email.exs
+++ priv/repo/migrations/20240911064812_add_unique_index_to_users_email.exs
@@ -1,7 +1,20 @@
 defmodule Galaxy.Repo.Migrations.AddUniqueIndexToUsersEmail do
   use Ecto.Migration
 
+
   def change do
-   create unique_index(:users, [:email])
+    execute("""
+      DO $$
+      BEGIN
+        IF NOT EXISTS (
+          SELECT 1
+          FROM pg_class c
+          JOIN pg_namespace n ON n.oid = c.relnamespace
+          WHERE c.relname = 'users_email_index'
+        ) THEN
+          CREATE UNIQUE INDEX users_email_index ON users(email);
+        END IF;
+      END$$;
+    """)
   end
 end
