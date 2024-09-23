0d0c0a71d2ffc36e46795e08ac60061e836558fd
G-ordon <gordonochieng5454@gmail.com>
2024-09-23 14:08:04 +0300

    generate pressence

---
+++ lib/galaxy_web/channels/presence.ex
@@ -0,0 +1,11 @@
+defmodule GalaxyWeb.Presence do
+  @moduledoc """
+  Provides presence tracking to channels and processes.
+
+  See the [`Phoenix.Presence`](https://hexdocs.pm/phoenix/Phoenix.Presence.html)
+  docs for more details.
+  """
+  use Phoenix.Presence,
+    otp_app: :galaxy,
+    pubsub_server: Galaxy.PubSub
+end
